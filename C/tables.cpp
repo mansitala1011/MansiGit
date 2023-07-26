901405ef539ed1c2c1c54f9700c5ac916d5bf773
mansi tala <202021bca1919.mansi@gmail.com>
2023-07-26 15:40:46 +0530

    first

--- C/tables.cpp
+++ C/tables.cpp
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main()
 {
-	int n,i;
-	for(i=1;i<=10;i++)
-	
-	for(n=1;n<=10;n++)
-	
-	int product= i *n;
-	printf("%d"*)
-	{
-		printf("%d*%d=%d\n",n,i,n*i);
-	}
-	return 0;
+   int i,j;
+   for(i=1;i<=10;i++)
+   {
+     for(j=1;j<=10;j++)
+     {
+     	int product=i*j;
+	    printf("%d*%d = %d\n ",i,j,product);
+     }
+     printf("\n");
+    }
+    return 0;
 }
